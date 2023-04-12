44a81e1484ea92a6b93f1c627ffe92818e62632e
= <danielcf.vix@hotmail.com>
2023-04-12 18:18:36 -0300

    feat:creatInputAndSelect

---
+++ my-app/src/components/input/style.input.ts
@@ -0,0 +1,87 @@
+import styled, { css } from "styled-components";
+import { iStyleProps } from "../../interface/styled";
+
+const Input = styled.input`
+  ${({ font }: iStyleProps) => {
+    switch (font) {
+      case "regular-input":
+        return css`
+          width: 315px;
+          height: 48px;
+          padding: 0px 16px 0px 16px;
+          border-radius: 4px;
+          background-color: ${({ theme }) => theme.colors.grey.g9};
+          transition: 0.5s;
+          margin-top: 1rem;
+          color: ${({ theme }) => theme.colors.whiteFixed};
+          font-size: ${({ theme }) => theme.typography.fontSize.size16px};
+          font-weight: ${({ theme }) => theme.typography.fontWeight.weight400};
+          border: 1px solid ${({ theme }) => theme.colors.grey.g5};
+          outline: none;
+          &::placeholder {
+            color: ${({ theme }) => theme.colors.grey.g3};
+          }
+          &:focus {
+            border: 1px solid ${({ theme }) => theme.colors.brand.b2};
+          }
+          &:hover {
+            filter: brightness(0.9);
+          }
+        `;
+      case "large-input":
+        return css`
+          width: 315px;
+          height: 80px;
+          padding: 24px 16px 24px 16px;
+          border-radius: 4px;
+          background-color: ${({ theme }) => theme.colors.grey.g9};
+          transition: 0.5s;
+          margin-top: 1rem;
+          color: ${({ theme }) => theme.colors.whiteFixed};
+          font-size: ${({ theme }) => theme.typography.fontSize.size16px};
+          font-weight: ${({ theme }) => theme.typography.fontWeight.weight400};
+          border: 1px solid ${({ theme }) => theme.colors.grey.g5};
+          outline: none;
+          &::placeholder {
+            color: ${({ theme }) => theme.colors.grey.g3};
+          }
+          &:focus {
+            border: 1px solid ${({ theme }) => theme.colors.brand.b2};
+          }
+          &:hover {
+            filter: brightness(0.9);
+          }
+        `;
+    }
+  }}
+`;
+
+const Select = styled.select`
+  ${({ font }: iStyleProps) => {
+    switch (font) {
+      case "regular-select":
+        return css`
+          width: 315px;
+          height: 48px;
+          padding: 0px 16px 0px 16px;
+          border-radius: 4px;
+          background-color: ${({ theme }) => theme.colors.grey.g9};
+          transition: 0.8s;
+          margin-top: 1rem;
+          color: ${({ theme }) => theme.colors.whiteFixed};
+          font-size: ${({ theme }) => theme.typography.fontSize.size16px};
+          font-weight: ${({ theme }) => theme.typography.fontWeight.weight400};
+          border: 1px solid ${({ theme }) => theme.colors.grey.g5};
+          outline: none;
+          &::placeholder {
+            color: ${({ theme }) => theme.colors.grey.g3};
+          }
+          &:hover {
+            filter: brightness(0.9);
+          }
+        `;
+    }
+  }}
+`;
+
+export { Input, Select };
