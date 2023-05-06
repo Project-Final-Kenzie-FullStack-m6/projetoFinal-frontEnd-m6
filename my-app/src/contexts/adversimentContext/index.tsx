b950e52fe77d275287d0977318f69f3d7eb1d2af
Nikson <niksonpoke@gmail.com>
2023-05-06 15:44:03 -0300

    feat/ filters max-min KM and Price

--- my-app/src/contexts/adversimentContext/index.tsx
+++ my-app/src/contexts/adversimentContext/index.tsx
@@ -26,30 +26,23 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [activeFilter, setActiveFilter] = useState(false);
   const [listBrands, setListBrands] = useState([]);
-  const [detailsAds, setDetailsAds] = useState({} as iAdversimentDataUpdate)
+  const [detailsAds, setDetailsAds] = useState({} as iAdversimentDataUpdate);
   const [isActive, setIsActive] = useState(true);
 
   const navigate = useNavigate();
 
-  // const arrBrands: any = [];
-  // adversimentData.map((data) => {
-  //   if (!arrBrands.includes(data.brand)) {
-  //     arrBrands.push(data.brand);
-  //   }
-  // });
-  // setListBrands(arrBrands);
-
   const loadAdversiment = async () => {
     try {
       const { data } = await Api.get("/adversiments");
-      setAdversimentData(data);
-      setFilterAdversiments(data);
+      const dataOrderMin = orderMinPrice(orderMinKM(data));
+      setAdversimentData(dataOrderMin);
+      setFilterAdversiments(dataOrderMin);
       if (filterBrand) {
         if (activeFilter) {
           const listFilter = adversimentData.filter((data) => data.brand === filterBrand);
           setFilterAdversiments(listFilter);
         } else {
-          setAdversimentData(data);
+          setAdversimentData(dataOrderMin);
           setFilterBrand("");
         }
       } else if (filterModel) {
@@ -57,7 +50,7 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
           const listFilter = adversimentData.filter((data) => data.model === filterModel);
           setFilterAdversiments(listFilter);
         } else {
-          setAdversimentData(data);
+          setAdversimentData(dataOrderMin);
           setFilterModel("");
         }
       } else if (filterColor) {
@@ -65,7 +58,7 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
           const listFilter = adversimentData.filter((data) => data.color === filterColor);
           setFilterAdversiments(listFilter);
         } else {
-          setAdversimentData(data);
+          setAdversimentData(dataOrderMin);
           setFilterColor("");
         }
       } else if (filterAge) {
@@ -73,7 +66,7 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
           const listFilter = adversimentData.filter((data) => data.age === filterAge);
           setFilterAdversiments(listFilter);
         } else {
-          setAdversimentData(data);
+          setAdversimentData(dataOrderMin);
           setFilterAge(0);
         }
       } else if (filterFuel) {
@@ -81,11 +74,15 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
           const listFilter = adversimentData.filter((data) => data.fuelType === filterFuel);
           setFilterAdversiments(listFilter);
         } else {
-          setAdversimentData(data);
+          setAdversimentData(dataOrderMin);
           setFilterFuel("");
         }
       } else if (filterKM) {
+        const dataOrderMax = orderMaxKM(filterAdversiments);
+        setFilterAdversiments(dataOrderMax);
       } else if (filterPrice) {
+        const dataOrderMax = orderMaxPrice(filterAdversiments);
+        setFilterAdversiments(dataOrderMax);
       } else {
       }
 
@@ -96,21 +93,105 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
     setLoading(false);
   };
 
-  //   const loadAdversiment = async () => {
-  //     try {
-  //       const { data } = await Api.get("/adversiments");
-  //       setAdversimentData(data);
-  //       setLoading(true);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //     setLoading(false);
-  //   };
+  const compareNum = (a: any, b: any) => {
+    if (a === b) return 0;
+    if (a < b) return -1;
+    if (a > b) return 1;
+  };
+
+  const orderMinKM = (listAdd: any) => {
+    const arrOfKM: any = [];
+    const arrExtra: any = [];
+    const minOrderKM: any = [];
+
+    listAdd.map((item: any) => {
+      if (!arrOfKM.includes(item.mileAge)) {
+        arrOfKM.push(item.mileAge);
+      }
+    });
+
+    arrOfKM.sort(compareNum);
+    console.log(arrOfKM);
+
+    for (let i = 0; i < arrOfKM.length; i++) {
+      listAdd.map((item: any) => {
+        if (item.mileAge === arrOfKM[i]) {
+          minOrderKM.push(item);
+        }
+      });
+    }
+
+    return minOrderKM;
+  };
+
+  const orderMaxKM = (listAdd: any) => {
+    const arrOfKM: any = [];
+    const maxOrderKM: any = [];
+
+    listAdd.map((item: any) => {
+      if (!arrOfKM.includes(item.mileAge)) {
+        arrOfKM.push(item.mileAge);
+      }
+    });
+    arrOfKM.sort(compareNum).reverse();
+
+    for (let i = 0; i < arrOfKM.length; i++) {
+      listAdd.map((item: any) => {
+        if (item.mileAge === arrOfKM[i]) {
+          maxOrderKM.push(item);
+        }
+      });
+    }
+
+    return maxOrderKM;
+  };
+
+  const orderMinPrice = (listAdd: any) => {
+    const arrOfPrice: any = [];
+    const minOrderPrice: any = [];
+
+    listAdd.map((item: any) => {
+      if (!arrOfPrice.includes(item.price)) {
+        arrOfPrice.push(item.price);
+      }
+    });
+    arrOfPrice.sort();
+
+    for (let i = 0; i < arrOfPrice.length; i++) {
+      listAdd.map((item: any) => {
+        if (item.price === arrOfPrice[i]) {
+          minOrderPrice.push(item);
+        }
+      });
+    }
+
+    return minOrderPrice;
+  };
+
+  const orderMaxPrice = (listAdd: any) => {
+    const arrOfPrice: any = [];
+    const maxOrderPrice: any = [];
+
+    listAdd.map((item: any) => {
+      if (!arrOfPrice.includes(item.price)) {
+        arrOfPrice.push(item.price);
+      }
+    });
+    arrOfPrice.sort().reverse();
+
+    for (let i = 0; i < arrOfPrice.length; i++) {
+      listAdd.map((item: any) => {
+        if (item.price === arrOfPrice[i]) {
+          maxOrderPrice.push(item);
+        }
+      });
+    }
+
+    return maxOrderPrice;
+  };
 
   useEffect(() => {
     loadAdversiment();
-    // setListBrands(arrBrands);
-    // }, []);
   }, [
     filterBrand,
     filterModel,
@@ -122,28 +203,16 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
     activeFilter,
   ]);
 
-  // const loadOnlyMyAdversiment = async () => {
-  //     try {
-  //         const {data} = await Api.get("/adversiments")
-  //         setAdversimentData(data)
-  //         setLoading(true)
-  //     } catch (error) {
-  //         console.error(error)
-  //     }
-  //     setLoading(false)
-  // }
-
   const getDetailsAdversiment = async (id: string | undefined) => {
     try {
-      const {data} = await Api.get(`/adversiments/${id}`)
-      setDetailsAds(data)
+      const { data } = await Api.get(`/adversiments/${id}`);
+      setDetailsAds(data);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   const postNewAdversiment = async (data: iAdversimentDataRegister) => {
-
     try {
       await Api.post("/adversiments", data);
       loadAdversiment();
@@ -156,50 +225,45 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
   };
 
   const updateAdversiment = async (data: iAdversimentDataUpdate) => {
-    const token = localStorage.getItem("token")
-    const newData = {...data, isActive: isActive}
-  
+    const token = localStorage.getItem("token");
+    const newData = { ...data, isActive: isActive };
+
     try {
       const response = await Api.patch(`/adversiments/${detailsAds?.id}`, newData, {
         headers: {
-          Authorization: `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
       });
-      window.location.reload()
-      console.log(response.data)
-
+      window.location.reload();
+      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  const createCommentUser = async(data: iCommentDataRequest) =>{
-    const token = localStorage.getItem("token")
+  const createCommentUser = async (data: iCommentDataRequest) => {
+    const token = localStorage.getItem("token");
     try {
-      const response = await Api.post(`/comments/${detailsAds.id}`, data,{
+      const response = await Api.post(`/comments/${detailsAds.id}`, data, {
         headers: {
-          Authorization: `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
-      })
-      navigate("/details")
-      console.log(response.data)
+      });
+      navigate("/details");
+      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
-  }
-
+  };
 
   const deleteAdversiment = async () => {
-    
     try {
       await Api.delete(`/adversiments/${detailsAds?.id}`);
-      window.location.reload()
+      window.location.reload();
     } catch (error) {
       console.error(error);
     }
-  }
-
-
+  };
 
   return (
     <AdversimentContext.Provider
@@ -235,7 +299,7 @@ const AdversimentProvider = ({ children }: iAdversimentProviderProps) => {
         filterAdversiments,
         setFilterAdversiments,
         listBrands,
-        isActive, 
+        isActive,
         setIsActive,
       }}
     >
